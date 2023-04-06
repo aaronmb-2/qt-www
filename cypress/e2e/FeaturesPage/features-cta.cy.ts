0fd9cf4400b54b32e3a7f85c0ee82d0ca8be4a23
Joris Jansen <joris97jansen@gmail.com>
2023-04-06 11:00:35 +0200

    Fix failing Cypress test && remove continue on error

--- cypress/e2e/FeaturesPage/features-cta.cy.ts
+++ cypress/e2e/FeaturesPage/features-cta.cy.ts
@@ -2,7 +2,7 @@
 
 describe("Check Home Page brokers components", () => {
   it("Should show the 6 connected brokers", () => {
-    cy.visit("localhost:3000");
-    cy.get("#home-brokers a").should("have.length", 2);
+    cy.visit("localhost:3000/features");
+    cy.get("#features-cta a").should("have.length", 2);
   });
 });
