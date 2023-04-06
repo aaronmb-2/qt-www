8e920e611f046e12cffb1e5fc321f75bcaf0e426
Joris Jansen <joris97jansen@gmail.com>
2023-04-06 11:13:32 +0200

    Try fix failing Cypress test

--- cypress/e2e/FeaturesPage/features-cta.cy.ts
+++ cypress/e2e/FeaturesPage/features-cta.cy.ts
@@ -2,7 +2,7 @@
 
 describe("Check Home Page brokers components", () => {
   it("Should show the 6 connected brokers", () => {
-    cy.visit("localhost:3000/features");
+    cy.visit("http://localhost:3000/features");
     cy.get("#features-cta a").should("have.length", 2);
   });
 });
