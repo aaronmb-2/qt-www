e69f6489443604b1e8840e3f9b1f5f0aaf0ef461
Joris Jansen <joris97jansen@gmail.com>
2023-04-06 08:20:00 +0200

    Fix ESLint issues

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    setupNodeEvents(_, __) {
       // implement node event listeners here
     },
   },
