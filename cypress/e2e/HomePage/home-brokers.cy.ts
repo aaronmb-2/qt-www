aa77f40aa6d1a862b6379ebc02e21575276b4140
Joris Jansen <joris97jansen@gmail.com>
2023-04-06 08:17:58 +0200

    Remove vitest from ESLint

--- cypress/e2e/HomePage/home-brokers.cy.ts
+++ cypress/e2e/HomePage/home-brokers.cy.ts
@@ -1,8 +1,8 @@
 /// <reference types="Cypress" />
 
-describe('Check Home Page brokers components', () => {
-  it('Should show the 6 connected brokers', () => {
-    cy.visit('localhost:3000')
-    cy.get("#home-brokers img").should('have.length', 6)
-  })
-})
+describe("Check Home Page brokers components", () => {
+  it("Should show the 6 connected brokers", () => {
+    cy.visit("localhost:3000");
+    cy.get("#home-brokers img").should("have.length", 6);
+  });
+});
