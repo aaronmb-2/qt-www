74c80085cb234d1d50ea8b27c855cddd75115dab
Joris Jansen <joris97jansen@gmail.com>
2023-04-09 22:24:44 +0200

    Format TF

--- .tf/modules/cloud_front/data.tf
+++ .tf/modules/cloud_front/data.tf
@@ -3,19 +3,19 @@
 ########################################
 
 locals {
-  distribution_enabled = true
+  distribution_enabled             = true
   distribution_default_root_object = "/home/index.html"
-  distribution_allowed_methods = ["GET", "HEAD"]
+  distribution_allowed_methods     = ["GET", "HEAD"]
 
   forward_query_string = false
-  cookie_forward = "none"
+  cookie_forward       = "none"
 
   viewer_protocol_policy = "redirect-to-https"
-  min_ttl = 0
-  default_ttl = 3600
-  max_ttl = 86400
+  min_ttl                = 0
+  default_ttl            = 3600
+  max_ttl                = 86400
 
-  geo_restriction_type = "whitelist"
+  geo_restriction_type      = "whitelist"
   geo_restriction_locations = ["US", "CA", "GB", "DE", "NL"]
 
   certificate_default = true
