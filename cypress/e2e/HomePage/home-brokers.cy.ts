3ab1bd00c6ca673f175cad97b3aab134d62168d0
Joris Jansen <joris97jansen@gmail.com>
2023-04-10 12:26:01 +0200

    Set URL for Cypress

--- cypress/e2e/HomePage/home-brokers.cy.ts
+++ cypress/e2e/HomePage/home-brokers.cy.ts
@@ -2,7 +2,7 @@
 
 describe("Check Home Page brokers components", () => {
   it("Should show the 6 connected brokers", () => {
-    cy.visit("localhost:3000");
+    cy.visit("localhost:3000/home");
     cy.get("#home-brokers img").should("have.length", 6);
   });
 });
