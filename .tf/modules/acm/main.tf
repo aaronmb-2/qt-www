062355e9771157ef04f3ceb327109e2ae15f49d4
Joris Jansen <joris97jansen@gmail.com>
2023-04-12 13:22:02 +0200

    Implement Certificated validation && Adding to Route53 records 2.0

--- .tf/modules/acm/main.tf
+++ .tf/modules/acm/main.tf
@@ -15,13 +15,30 @@ resource "aws_acm_certificate" "main" {
 #   records        = ["dev.example.com"]
 # }
 
-resource "aws_route53_record" "main" {
+# resource "aws_route53_record" "main" {
+#   allow_overwrite = true
+#   name            = tolist(aws_acm_certificate.main.domain_validation_options)[0].resource_record_name
+#   records         = [tolist(aws_acm_certificate.main.domain_validation_options)[0].resource_record_value]
+#   type            = tolist(aws_acm_certificate.main.domain_validation_options)[0].resource_record_type
+#   zone_id         = var.hosted_zone_name
+#   ttl             = 60
+# }
+
+resource "aws_route53_record" "example" {
+  for_each = {
+    for dvo in aws_acm_certificate.main.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
   allow_overwrite = true
-  name            = tolist(aws_acm_certificate.main.domain_validation_options)[0].resource_record_name
-  records         = [tolist(aws_acm_certificate.main.domain_validation_options)[0].resource_record_value]
-  type            = tolist(aws_acm_certificate.main.domain_validation_options)[0].resource_record_type
-  zone_id         = var.hosted_zone_name
+  name            = each.value.name
+  records         = [each.value.record]
   ttl             = 60
+  type            = each.value.type
+  zone_id         = var.hosted_zone_name
 }
 
 
