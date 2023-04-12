803981961aa12ad061a1f4226de891bd770cebeb
Joris Jansen <joris97jansen@gmail.com>
2023-04-12 12:11:53 +0200

    Implement Certificated validation && Adding to Route53 records

--- .tf/modules/acm/main.tf
+++ .tf/modules/acm/main.tf
@@ -7,12 +7,30 @@ resource "aws_acm_certificate" "main" {
   validation_method         = local.certificate_validation_method
 }
 
+# resource "aws_route53_record" "env" {
+#   zone_id = var.hosted_zone_name
+#   type    = "CNAME"
+#   ttl     = 60
+#   name    = aws_acm_certificate.main.domain_name.resource
+#   records        = ["dev.example.com"]
+# }
+
+resource "aws_route53_record" "main" {
+  allow_overwrite = true
+  name            = tolist(aws_acm_certificate.main.domain_validation_options)[0].resource_record_name
+  records         = [tolist(aws_acm_certificate.main.domain_validation_options)[0].resource_record_value]
+  type            = tolist(aws_acm_certificate.main.domain_validation_options)[0].resource_record_type
+  zone_id         = var.hosted_zone_name
+  ttl             = 60
+}
+
+
 resource "aws_acm_certificate_validation" "env" {
   certificate_arn         = aws_acm_certificate.main.arn
   validation_record_fqdns = [var.route53_record_env_fqdn]
 }
 
-resource "aws_acm_certificate_validation" "www-env" {
+resource "aws_acm_certificate_validation" "www_env" {
   certificate_arn         = aws_acm_certificate.main.arn
   validation_record_fqdns = [var.route53_record_www_env_fqdn]
 }
---
+++ middleware/redirect.ts
@@ -0,0 +1,5 @@
+export default defineNuxtRouteMiddleware((_, __) => {
+  return navigateTo({
+    path: "/home",
+  });
+});
